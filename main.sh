18de0c53fd8317166661efdad462cd01eee1d83d
Ivan <iv4n9f@gmail.com>
2024-01-29 08:44:23 +0100

    0.02
    Relative routes to absolute

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dir=$(pwd)
+user=$(whoami)
 
 sudo apt update
 sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh-y
@@ -11,19 +12,19 @@ git clone https://github.com/baskerville/sxhkd.git
 cd bspwm && make && sudo make install
 cd ../sxhkd && make && sudo make install
 
-mkdir -p ~/.config/{bspwm,sxhkd,polybar}
-mkdir ~/.config/bspwm/scripts/
+mkdir -p /home/$user/.config/{bspwm,sxhkd,polybar}
+mkdir /home/$user/.config/bspwm/scripts/
 
-cp $dir/bspwmrc ~/.config/bspwm/
-cp $dir/sxhkdrc ~/.config/sxhkd/
-cp $dir/config.ini ~/.config/polybar/
-cp $dir/launch.sh ~/.config/polybar/
-cp $dir/bspwm_resize ~/.config/bspwm/scripts/bspwm_resize
-cp $dir/wallpaper.jpeg ~/Pictures/wallpaper.jpeg
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+cp $dir/bspwmrc /home/$user/.config/bspwm/
+cp $dir/sxhkdrc /home/$user/.config/sxhkd/
+cp $dir/config.ini /home/$user/.config/polybar/
+cp $dir/launch.sh /home/$user/.config/polybar/
+cp $dir/bspwm_resize /home/$user/.config/bspwm/scripts/bspwm_resize
+cp $dir/wallpaper.jpeg /home/$user/Pictures/wallpaper.jpeg
+cp /etc/X11/xinit/xinitrc /home/$user/.xinitrc
 
-echo "exec bspwm" >> ~/.xinitrc
+echo "exec bspwm" >> /home/$user/.xinitrc
 
-chmod u+x ~/.config/bspwm/bspwmrc
-chmod u+x ~/.config/polybar/launch.sh
-chmod u+x ~/.config/bspwm/scripts/bspwm_resize
+chmod u+x /home/$user/.config/bspwm/bspwmrc
+chmod u+x /home/$user/.config/polybar/launch.sh
+chmod u+x /home/$user/.config/bspwm/scripts/bspwm_resize
