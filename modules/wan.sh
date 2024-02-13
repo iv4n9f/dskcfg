4d850205bf9a69482256c22659df5df2ce7314c9
Ivan <iv4n9f@gmail.com>
2024-02-13 17:45:03 +0100

    0.07
    Get the wan module

--- modules/wan.sh
+++ modules/wan.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-interface=$1
-
-if [[ -n $interface ]]; then
-  ip=$(ip -4 addr show dev $interface | awk '/inet /{print $2}')
-  if [[ -n $ip ]]; then
-    echo -e "%{F#0F0} 󰲝 %{F-}${interface^^} $ip"
-  else
-    echo -e "%{F#F00} 󰲝 %{F-}${interface^^} $ip"
-  fi
-  echo -e "%{F#F00} 󰲝 %{F-}${interface^^} $ip"
-fi
-
-
+wan=$(curl -s https://api.myip.com | jq -r '.ip')
+echo -e "%{F#0F0}  %{F-} $wan"
