a41b326b96fc46ffbab107f60fe2e5a7a72957dc
Ivan <iv4n9f@gmail.com>
2024-02-13 19:03:33 +0100

    0.12
    added personal config

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+password="password"
 dir=$(pwd)
 user=$(whoami)
 
 # Initial Enviroment
 
 sudo apt update
-sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors jq -y
+sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq -y
 
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
@@ -25,6 +26,9 @@ cp $dir/launch.sh /home/$user/.config/polybar/
 cp -r $dir/modules/ /home/$user/.config/polybar/
 cp $dir/bspwm_resize /home/$user/.config/bspwm/scripts/bspwm_resize
 cp $dir/wallpaper.png /home/$user/Pictures/wallpaper.png
+sudo cp $dir/set_target /usr/bin/set_target
+sudo cp $dir/btiwarden /usr/bin/btiwarden
+sudo cp $dir/brave /usr/bin/brave
 cp /etc/X11/xinit/xinitrc /home/$user/.xinitrc
 
 echo "exec bspwm" >> /home/$user/.xinitrc
@@ -33,6 +37,9 @@ chmod u+x /home/$user/.config/bspwm/bspwmrc
 chmod u+x /home/$user/.config/polybar/launch.sh
 chmod u+x /home/$user/.config/bspwm/scripts/bspwm_resize
 chmod +x /home/$user/.config/polybar/modules/*.sh
+sudo chmod +x /usr/bin/set_target
+sudo chmod +x /usr/bin/btiwarden
+sudo chmod +x /usr/bin/brave
 
 # Fonts
 
@@ -49,3 +56,13 @@ sudo systemctl enable snapd.socket && sudo systemctl start snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install code --classic
 sudo snap install brave bitwarden
+
+# Personal Config
+
+echo $password > /home/$user/Credentials/bit.pas
+mkdir /home/$user/Credentials /home/$user/Projects /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
+sudo chown -R $user:$user /home/$user/Credentials /home/$user/Projects /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
+mkdir /home/$user/Credentials/.tmp /home/$user/Credentials/.keys
+openssl genpkey -algorithm RSA -out /home/$user/Credentials/.keys/private_key.pem
+openssl rsa -pubout -in /home/$user/Credentials/.keys/private_key.pem -out /home/$user/Credentials/.keys/public_key.pem
+openssl pkeyutl -encrypt -pubin -inkey /home/$user/Credentials/.keys/public_key.pem -in /home/$user/Credentials/bit.pas -out /home/$user/Credentials/bit.enc && rm -r /home/$user/Credentials/bit.pas
