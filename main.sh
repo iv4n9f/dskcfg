120ca72796f645150ed957fcaf45af06516b4daa
Ivan <iv4n9f@gmail.com>
2024-02-14 09:56:10 +0100

    0.26
    Some minor corrections

--- main.sh
+++ main.sh
@@ -7,9 +7,13 @@ timezone="Europe/Madrid"
 
 # Initial Enviroment
 
-sudo apt update
-sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl -y
+timedatectl set-timezone $timezone
 
+mkdir /home/$user/Credentials /home/$user/Projects /home/$user/Machines /home/$user/Software /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
+sudo chown -R $user:$user /home/$user/Credentials /home/$user/Projects /home/$user/Machines /home/$user/Software /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
+
+sudo apt update
+sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl bat -y
 
 cd /home/$user/Downloads
 
@@ -30,9 +34,9 @@ cp $dir/launch.sh /home/$user/.config/polybar/
 cp -r $dir/modules/ /home/$user/.config/polybar/
 cp $dir/bspwm_resize /home/$user/.config/bspwm/scripts/bspwm_resize
 cp $dir/wallpaper.png /home/$user/Pictures/wallpaper.png
-sudo cp $dir/set_target /usr/bin/set_target
-sudo cp $dir/bitwarden /usr/bin/bitwarden
-sudo cp $dir/brave /usr/bin/brave
+sudo cp $dir/utils/set_target /usr/bin/set_target
+sudo cp $dir/utils/bitwarden /usr/bin/bitwarden
+sudo cp $dir/utils/brave /usr/bin/brave
 cp /etc/X11/xinit/xinitrc /home/$user/.xinitrc
 
 echo "exec bspwm" >> /home/$user/.xinitrc
@@ -78,8 +82,6 @@ sudo apt install brave-browser -y
 
 # Personal Config
 
-mkdir /home/$user/Credentials /home/$user/Projects /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
-sudo chown -R $user:$user /home/$user/Credentials /home/$user/Projects /home/$user/Downloads /home/$user/Pictures /home/$user/Videos /home/$user/Music /home/$user/Documents /home/$user/.ssh
 echo "$password" > /home/$user/Credentials/bit.pas
 mkdir -p /home/$user/Credentials/.tmp /home/$user/Credentials/.keys
 openssl genpkey -algorithm RSA -out /home/$user/Credentials/.keys/private_key.pem
@@ -90,6 +92,8 @@ set_target localhost
 rm -r /home/$user/Downloads/bspwm /home/$user/Downloads/sxhkd /home/$user/Downloads/rofi-themes-collection /home/$user/Downloads/Hack.zip
 
 sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.old
+sudo cp /etc/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf.old
 sudo sed -i 's/#autologin-user=/autologin-user='$user'/g' /etc/lightdm/lightdm.conf
-sudo sed -i 's/theme-name=Adwaita/theme-name=elementary Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
-sudo sed -i 's/background=/usr/share/images/desktop-base/login-background.svg//home/$user/Pictures/wallpaper.png/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/theme-name=Kali Light/theme-name=elementary Dark/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/font-name=Cantarell 11/font-name=Hack Nerd Font 10/g' /etc/lightdm/lightdm-gtk-greeter.conf
+sudo sed -i 's/"background=/usr/share/images/desktop-base/login-background.svg"/"/home/$user/Pictures/wallpaper.png"/g' /etc/lightdm/lightdm-gtk-greeter.conf
--- modules/ip.sh
+++ modules/ip.sh
@@ -3,9 +3,8 @@
 interface=$1
 
 if [[ -n $interface ]]; then
-  ip=$(ip -4 addr show dev $interface 2> /dev/null | awk '/inet /{print $2}')
+  ip=$(ip -4 addr show dev $interface 2> /dev/null | awk '/inet /{print $2}' | cut -d "/" -f 1)
   if [[ -n $ip ]]; then
-    ip=${echo $ip | cut -d / -f 1}
     echo -e "%{F#0F0} 󰲝 %{F-}${interface^^} $ip"
   else
     echo -e "%{F#F00} 󰲝 %{F-}${interface^^} $ip"
