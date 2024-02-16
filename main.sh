1d5e0bfc0397dd1de856300a4c0d1461d78e858b
Ivan <iv4n9f@gmail.com>
2024-02-16 08:45:40 +0100

    0.43
    Changed Bitwarden to keepasxc and brave to tor-browser

--- main.sh
+++ main.sh
@@ -19,8 +19,6 @@ user=$(whoami)
 # Initial Setup and installs
 
 sudo timedatectl set-timezone $timezone
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list 
 sudo apt install wget gpg apt-transport-https -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
@@ -29,7 +27,7 @@ rm -f packages.microsoft.gpg
 
 
 sudo apt update
-sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl bat snapd brave-browser code davfs2 -y
+sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl bat snapd -browser code davfs2 keepasxc torbrowser-launcher -y
 sudo systemctl enable snapd.socket && sudo systemctl start snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
@@ -50,7 +48,6 @@ git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip
 git clone https://github.com/lr-tech/rofi-themes-collection.git
-wget "https://github.com/bitwarden/clients/releases/download/desktop-v2022.5.1/Bitwarden-2022.5.1-amd64.deb"
 
 # Install
 
@@ -63,7 +60,6 @@ mkdir -p ~/.local/share/rofi/themes/
 cp themes/rounded-common.rasi ~/.local/share/rofi/themes/
 cp themes/rounded-blue-dark.rasi ~/.local/share/rofi/themes/
 cd /home/$user/Downloads
-sudo dpkg -i Bitwarden*.deb
 
 # Configs
 
@@ -77,8 +73,8 @@ cp $dir/images/wallpaper.png /home/$user/Pictures/wallpaper.png
 cp $dir/images/wallpaper.png /usr/share/pictures/wallpaper.png
 sudo cp $dir/config/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 sudo cp $dir/utils/set_target /usr/bin/set_target
-sudo cp $dir/utils/bitw /usr/bin/bitw
-sudo cp $dir/utils/brave /usr/bin/brave
+sudo cp $dir/utils/keepass /usr/bin/keepass
+sudo cp $dir/utils/torbrowser /usr/bin/torbrowser
 sudo cp $dir/utils/mount_dav /usr/bin/mount_dav
 cp /etc/X11/xinit/xinitrc /home/$user/.xinitrc
 echo "exec bspwm" >> /home/$user/.xinitrc
@@ -90,8 +86,8 @@ chmod u+x /home/$user/.config/polybar/launch.sh
 chmod u+x /home/$user/.config/bspwm/scripts/bspwm_resize
 chmod +x /home/$user/.config/polybar/modules/*.sh
 sudo chmod +x /usr/bin/set_target
-sudo chmod +x /usr/bin/bitw
-sudo chmod +x /usr/bin/brave
+sudo chmod +x /usr/bin/keepass
+sudo chmod +x /usr/bin/torbrowser
 sudo chmod +x /usr/bin/mount_dav
 
 # Snaps installation
@@ -100,18 +96,18 @@ sudo chmod +x /usr/bin/mount_dav
 # Security
 
 if [[ $password != "password" ]]; then
-    echo "$password" > /home/$user/Credentials/bit.pas
+    echo "$password" > /home/$user/Credentials/kee.pas
     mkdir -p /home/$user/Credentials/.tmp /home/$user/Credentials/.keys
     openssl genpkey -algorithm RSA -out /home/$user/Credentials/.keys/private_key.pem
     openssl rsa -pubout -in /home/$user/Credentials/.keys/private_key.pem -out /home/$user/Credentials/.keys/public_key.pem
-    openssl pkeyutl -encrypt -pubin -inkey /home/$user/Credentials/.keys/public_key.pem -in /home/$user/Credentials/bit.pas -out /home/$user/Credentials/bit.enc && rm /home/$user/Credentials/bit.pas
+    openssl pkeyutl -encrypt -pubin -inkey /home/$user/Credentials/.keys/public_key.pem -in /home/$user/Credentials/kee.pas -out /home/$user/Credentials/kee.enc && rm /home/$user/Credentials/kee.pas
 else
     echo "You need to set a password"
 fi
 
 # Clean
 
-sudo rm -r /home/$user/Downloads/bspwm /home/$user/Downloads/sxhkd /home/$user/Downloads/rofi-themes-collection /home/$user/Downloads/Hack.zip /home/$user/Downloads/Bitwarden*.deb
+sudo rm -r /home/$user/Downloads/bspwm /home/$user/Downloads/sxhkd /home/$user/Downloads/rofi-themes-collection /home/$user/Downloads/Hack.zip
 
 # Additional config
 
