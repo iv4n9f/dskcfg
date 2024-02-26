ba5c2f2159ddf5c97ef8572c8473caab04bf982c
Ivan <iv4n9f@gmail.com>
2024-02-26 13:50:57 +0100

    Revert "0.48"
    This reverts commit 63d1dbf62efe701e9be9d0b0957135dd4371c2d5.

--- main.sh
+++ main.sh
@@ -3,16 +3,13 @@
 # Setup your config
 
 password="password"
-usb_key_uid="usb_key_uid"
 timezone="Europe/Madrid"
-usb_key_device=$(sudo blkid -sUUID | grep $usb_key_uid | cut -d ":" -f 1)
 
 # If you want setup davfs mount uncomment the following lines and set the variables
 # davfs_url="https://example.com"
 # davfs_user="example_user"
 # davfs_password="example_password"
 # davfs_mount_point="/mnt/example"
-# sudo apt-get install davfs2
 
 # Variables
 
@@ -31,7 +28,7 @@ rm -f packages.microsoft.gpg
 
 
 sudo apt update
-sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl bat snapd code keepassxc torbrowser-launcher -y
+sudo apt-get install make gcc libxcb-xinerama0-dev libxcb-icccm4-dev libxcb-randr0-dev libxcb-util0-dev libxcb-ewmh-dev libxcb-keysyms1-dev libxcb-shape0-dev polybar bspwm sxhkd rofi feh python3-pip net-tools gnome-terminal lm-sensors xclip jq wireguard resolvconf curl bat snapd  code davfs2 keepassxc torbrowser-launcher -y
 sudo systemctl enable snapd.socket && sudo systemctl start snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 
@@ -44,7 +41,6 @@ mkdir /home/$user/.config/bspwm/scripts/
 mkdir /home/$user/.config/polybar/modules/
 sudo mkdir -p /usr/share/pictures
 sudo mkdir -p $davfs_mount_point
-sudo mkdir -p /mnt/key
 
 # Download 
 
@@ -97,6 +93,7 @@ sudo chmod +x /usr/bin/mount_dav
 
 # Snaps installation
 
+<<<<<<< HEAD
 # Security
 
 if [[ $password != "password" ]]; then
@@ -111,6 +108,17 @@ if [[ $password != "password" ]]; then
     sudo openssl genpkey -algorithm RSA -out /mnt/key/.keys/private_key.pem
     sudo openssl rsa -pubout -in /mnt/key/.keys/private_key.pem -out /mnt/key/.keys/public_key.pem
     sudo openssl pkeyutl -encrypt -pubin -inkey /mnt/key/.keys/public_key.pem -in /home/$user/Credentials/kee.pas -out /home/$user/Credentials/kee.enc && rm /home/$user/Credentials/kee.pas
+=======
+
+# Security
+
+if [[ $password != "password" ]]; then
+    echo "$password" > /home/$user/Credentials/kee.pas
+    mkdir -p /home/$user/Credentials/.tmp /home/$user/Credentials/.keys
+    openssl genpkey -algorithm RSA -out /home/$user/Credentials/.keys/private_key.pem
+    openssl rsa -pubout -in /home/$user/Credentials/.keys/private_key.pem -out /home/$user/Credentials/.keys/public_key.pem
+    openssl pkeyutl -encrypt -pubin -inkey /home/$user/Credentials/.keys/public_key.pem -in /home/$user/Credentials/kee.pas -out /home/$user/Credentials/kee.enc && rm /home/$user/Credentials/kee.pas
+>>>>>>> parent of 6e06a33 (0.49)
 else
     echo "You need to set a password"
 fi
